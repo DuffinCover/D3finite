68908706e82c3c07d5a2fd12803f0e601a2df7a7
John Duffy <jduff814@gmail.com>
2022-11-05 20:39:44 -0600

    added brief TODOs to each of the js files, some organization

---
+++ js/satelliteDetail.sj
@@ -0,0 +1,5 @@
+class SatelliteDetails{
+    /*TODO
+    This needs a picture of that type of satellite 
+    */
+}
---
+++ js/worldView.js
@@ -0,0 +1,6 @@
+class Worldview{
+/*
+TODO
+this should show some sample of our satellites, as well as some image of the globe. (maybe it spins?) 
+*/
+}
