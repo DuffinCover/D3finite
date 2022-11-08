c497f9ac5a0b465da8d4e1b22b3d6d3fd8a97b1d
John Duffy <jduff814@gmail.com>
2022-11-07 23:33:47 -0700

    wft is going on. nothing appends

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@
 async function loadData() {
     // const satData = await d3.json('data/satellites.json');
     const satData = 'this is fake'
-    console.log('loaded')
-    let worldView = new Worldview(satData);
     return satData;
 }
+
+let satData = loadData()
+
+const worldView = new Worldview(satData);
 
 // let worldView = new Worldview('this is fake data');
--- js/worldView.js
+++ js/worldView.js
@@ -1,120 +1,58 @@
-class Worldview{
-/*
+class Worldview {
+  /*
 TODO
 this should show some sample of our satellites, as well as some image of the globe. (maybe it spins?) 
 */
-constructor(input_satellites){
+  constructor(input_satellites) {
     this.satellites = input_satellites;
-    
 
     // basic svg params
-    this.width = 300
-    this.height = 100
-    
+    this.width = 300;
+    this.height = 100;
+
     this.config = {
-        speed: 0.005,
-        verticalTilt: -30,
-        horizontalTilt: 0
-      }
+      speed: 0.005,
+      verticalTilt: -30,
+      horizontalTilt: 0,
+    };
 
-    let launchDensityScale = d3.scaleLinear()
-    // need actual json data to do stuff here, ill plan on something clever here. 
-    .domain(this.satellites, (d)=> console.log(d))
-    .range(['#fff2cd', '#990000']);
+    let launchDensityScale = d3
+      .scaleLinear()
+      // need actual json data to do stuff here, ill plan on something clever here.
+      .domain(this.satellites, (d) => console.log(d))
+      .range(["#fff2cd", "#990000"]);
 
-this.renderGlobe();
-}
+    this.renderGlobe();
+    // this.placeSatellites();
+  }
 
-// potential cool visualizaion? http://bl.ocks.org/emeeks/068ef3e4106e155467a3 
+  // potential cool visualizaion? http://bl.ocks.org/emeeks/068ef3e4106e155467a3
 
-renderGlobe(){
-
+  renderGlobe() {
     // https://bl.ocks.org/atanumallick/8d18989cd538c72ae1ead1c3b18d7b54
-    let locations = [];
-    let worldDiv = d3.select('#worldView')
-    
-    console.log('got here')
-    worldDiv
-    .append('svg')
-    .attr('width', width).attr('height', height);
-    // const markerGroup = svg.append('g');
+    console.log("entered renderGlobe");
 
+    let svg = d3
+      .select("#worldview")
+      .append("svg")
+      .attr("id", "circle")
+      .attr("width", this.width)
+      .attr("height", this.height);
 
+    svg
+    .enter()
+      .append("circle")
+      .attr("cx", 25)
+      .attr("cy", 25)
+      .attr("r", 25)
+      .style("fill", "purple");
 
-    // const projection = d3.geoOrthographic();
-    // const initialScale = projection.scale();
-    // const path = d3.geoPath().projection(projection);
-    // const center = [width/2, height/2];
+    console.log("made it past where i should be adding a circle");
+  }
 
-    // drawGlobe();    
-    // drawGraticule();
-    // enableRotation();    
+  sampleSatellites() {}
 
-    function drawGlobe() {  
-        d3.queue()
-            .defer(d3.json, 'https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/world-110m.json')          
-            // .defer(d3.json, 'locations.json')
-            .await((error, worldData, locationData) => {
-                svg.selectAll(".segment")
-                    .data(topojson.feature(worldData, worldData.objects.countries).features)
-                    .enter().append("path")
-                    .attr("class", "segment")
-                    .attr("d", path)
-                    .style("stroke", "#888")
-                    .style("stroke-width", "1px")
-                    .style("fill", (d, i) => '#e5e5e5')
-                    .style("opacity", ".6");
-                    // locations = locationData;
-                    drawMarkers();                   
-            });
-    }
-
-    function drawGraticule() {
-        const graticule = d3.geoGraticule()
-            .step([10, 10]);
-
-        svg.append("path")
-            .datum(graticule)
-            .attr("class", "graticule")
-            .attr("d", path)
-            .style("fill", "#fff")
-            .style("stroke", "#ccc");
-    }
-
-    function enableRotation() {
-        d3.timer(function (elapsed) {
-            projection.rotate([config.speed * elapsed - 120, config.verticalTilt, config.horizontalTilt]);
-            svg.selectAll("path").attr("d", path);
-            drawMarkers();
-        });
-    }        
-
-    function drawMarkers() {
-        const markers = markerGroup.selectAll('circle')
-            .data(locations);
-        markers
-            .enter()
-            .append('circle')
-            .merge(markers)
-            .attr('cx', d => projection([d.longitude, d.latitude])[0])
-            .attr('cy', d => projection([d.longitude, d.latitude])[1])
-            .attr('fill', d => {
-                const coordinate = [d.longitude, d.latitude];
-                gdistance = d3.geoDistance(coordinate, projection.invert(center));
-                return gdistance > 1.57 ? 'none' : 'steelblue';
-            })
-            .attr('r', 7);
-
-        markerGroup.each(function () {
-            this.parentNode.appendChild(this);
-        });
-    }
-}
-
-sampleSatellites(){
-
-}
-
-
-
+  placeSatellites() {
+    // http://bl.ocks.org/eesur/2ac63b3d0ece6682a42c0f9d3a6bfabc
+  }
 }
