27137f152fb77c7e3b5d56c4952fab2817d699cc
John Duffy <jduff814@gmail.com>
2022-11-09 16:58:29 -0700

    found some chart code to repurpose

--- js/script.js
+++ js/script.js
@@ -3,10 +3,22 @@ async function loadData() {
     const satData = await d3.json('data/satellites.json');
     return satData;
 }
+
+const globalState = {
+    satelliteData: null,
+    sampleSatellites: null,
+    table: null,
+    worldView: null,
+    lineChart: null,
+};
+
+
 loadData().then(data => {
     console.log(data);
-
-    let SatTable = new SatelliteTable(data);
+    globalState.satelliteData = data; 
+    // let SatTable = new SatelliteTable(data);
     let worldView = new Worldview(data);
+
+    globalState.worldView = worldView;
 });
 
--- js/worldView.js
+++ js/worldView.js
@@ -7,8 +7,9 @@ this should show some sample of our satellites, as well as some image of the glo
     this.satellites = input_satellites;
 
     // basic svg params
-    this.width = 300;
-    this.height = 100;
+    this.width = 500;
+    this.height = 500;
+    this.margin = 20;
 
     this.config = {
       speed: 0.005,
@@ -23,6 +24,7 @@ this should show some sample of our satellites, as well as some image of the glo
       .range(["#fff2cd", "#990000"]);
 
     this.renderGlobe();
+    
     // this.placeSatellites();
   }
 
@@ -30,26 +32,138 @@ this should show some sample of our satellites, as well as some image of the glo
 
   renderGlobe() {
     // https://bl.ocks.org/atanumallick/8d18989cd538c72ae1ead1c3b18d7b54
-    console.log("entered renderGlobe");
 
-    let svg = d3
+    // radial chart
+    // https://observablehq.com/@d3/radial-area-chart
+    const svg = d3
       .select("#worldview")
       .append("svg")
-      .attr("id", "circle")
       .attr("width", this.width)
-      .attr("height", this.height);
+      .attr("height", this.height)
+      .attr("viewBox", [
+        -this.width / 2,
+        -this.height / 2,
+        this.width,
+        this.height,
+      ])
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round");
 
-    svg
-      .append("circle")
-      .attr("cx", 25)
-      .attr("cy", 25)
-      .attr("r", 25)
-      .style("fill", "purple");
+    let innerRadius = this.width / 5;
+    let outerRadius = this.width / 2 - this.margin;
 
-    console.log("made it past where i should be adding a circle");
+    let sampleSats = this.sampleSatellites();
+    console.log(sampleSats)
+
+    let x = d3
+      .scaleUtc()
+      .domain([Date.UTC(2000, 0, 1), Date.UTC(2001, 0, 1) - 1])
+      .range([0, 2 * Math.PI]);
+
+    let y = d3
+      .scaleLinear()
+      .domain([d3.min(data, (d) => d.minmin), d3.max(data, (d) => d.maxmax)])
+      // .domain([500, 40000])
+      .range([innerRadius, outerRadius]);
+
+    
+
+    let xAxis = (g) =>
+      g
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .call((g) =>
+          g
+            .selectAll("g")
+            .data(x.ticks())
+            .join("g")
+            .each((d, i) => (d.id = "month"))
+            .call((g) =>
+              g
+                .append("path")
+                .attr("stroke", "#000")
+                .attr("stroke-opacity", 1)
+                .attr(
+                  "d",
+                  (d) => `
+              M${d3.pointRadial(x(d), innerRadius)}
+              L${d3.pointRadial(x(d), outerRadius)}
+            `
+                )
+            )
+            .call((g) =>
+              g
+                .append("path")
+                .attr("id", (d) => d.id.id)
+                .datum((d) => [d, d3.utcMonth.offset(d, 1)])
+                .attr("fill", "none")
+                .attr(
+                  "d",
+                  ([a, b]) => `
+              M${d3.pointRadial(x(a), innerRadius)}
+              A${innerRadius},${innerRadius} 0,0,1 ${d3.pointRadial(
+                    x(b),
+                    innerRadius
+                  )}
+            `
+                )
+            )
+            .call((g) =>
+              g
+                .append("text")
+                .append("textPath")
+                .attr("startOffset", 6)
+                .attr("xlink:href", (d) => d.id.href)
+                .text(d3.utcFormat("%B"))
+            )
+        );
+
+    let yAxis = (g) =>
+      g
+        .attr("text-anchor", "middle")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .call((g) =>
+          g
+            .selectAll("g")
+            .data(y.ticks().reverse())
+            .join("g")
+            .attr("fill", "none")
+            .call((g) =>
+              g
+                .append("circle")
+                .attr("stroke", "#000")
+                .attr("stroke-opacity", 0.2)
+                .attr("r", y)
+            )
+            .call((g) =>
+              g
+                .append("text")
+                .attr("y", (d) => -y(d))
+                .attr("dy", "0.35em")
+                .attr("stroke", "#fff")
+                .attr("stroke-width", 5)
+                .text((x, i) => `${x.toFixed(0)}${i ? "" : " km"}`)
+                .clone(true)
+                .attr("y", (d) => y(d))
+                .selectAll(function () {
+                  return [this, this.previousSibling];
+                })
+                .clone(true)
+                .attr("fill", "currentColor")
+                .attr("stroke", "none")
+            )
+        );
+
+    // svg.append("g").call(xAxis);
+
+    svg.append("g").call(yAxis);
   }
 
-  sampleSatellites() {}
+  async sampleSatellites() {
+    const satSampleData = await d3.json("data/satellites_sample.json");
+    return satSampleData;
+  }
 
   placeSatellites() {
     // http://bl.ocks.org/eesur/2ac63b3d0ece6682a42c0f9d3a6bfabc
