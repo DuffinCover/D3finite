416bdc045f425d2b0360cbca59aec9c70877c912
Aaron Schindler <legolover1000@gmail.com>
2022-11-11 12:49:37 -0700

    Added in mass column Attempted functionality of mass row

--- js/satelliteTable.js
+++ js/satelliteTable.js
@@ -7,16 +7,9 @@ class SatelliteTable{
      */
     constructor(data) {
         this.data = data;
-        // this.fakeData = [{"test1": "11", "test2": "12", "test3": "13"}, {"test1": "21", "test2": "22", "test3": "23"}, {"test1": "31", "test2": "32", "test3": "33"},
-        // {"test1": "11", "test2": "12", "test3": "13"}, {"test1": "21", "test2": "22", "test3": "23"}, {"test1": "31", "test2": "32", "test3": "33"},
-        // {"test1": "11", "test2": "12", "test3": "13"}, {"test1": "21", "test2": "22", "test3": "23"}, {"test1": "31", "test2": "32", "test3": "33"},
-        // {"test1": "11", "test2": "12", "test3": "13"}, {"test1": "21", "test2": "22", "test3": "23"}, {"test1": "31", "test2": "32", "test3": "33"},
-        // {"test1": "11", "test2": "12", "test3": "13"}, {"test1": "21", "test2": "22", "test3": "23"}, {"test1": "31", "test2": "32", "test3": "33"},
-        // {"test1": "11", "test2": "12", "test3": "13"}, {"test1": "21", "test2": "22", "test3": "23"}, {"test1": "31", "test2": "32", "test3": "33"},
-        // {"test1": "11", "test2": "12", "test3": "13"}, {"test1": "21", "test2": "22", "test3": "23"}, {"test1": "31", "test2": "32", "test3": "33"},
-        // {"test1": "11", "test2": "12", "test3": "13"}, {"test1": "21", "test2": "22", "test3": "23"}, {"test1": "31", "test2": "32", "test3": "33"},
-        // {"test1": "11", "test2": "12", "test3": "13"}, {"test1": "21", "test2": "22", "test3": "23"}, {"test1": "31", "test2": "32", "test3": "33"},
-        // {"test1": "11", "test2": "12", "test3": "13"}, {"test1": "21", "test2": "22", "test3": "23"}, {"test1": "31", "test2": "32", "test3": "33"}];
+        this.rowSvgWidth = 300;
+        this.rowSvgHeight = 50;
+        this.rowBarHeight = 40;
         
         // Stores table body selection and appends table rows
         let rowSelection = d3.select('#tableBody')
@@ -36,10 +29,36 @@ class SatelliteTable{
         // Adding in cell values
         cellSelect.filter(d => d.type === 'Name').text(d => d.value);
         cellSelect.filter(d => d.type === 'OriginCountry').text(d=>d.value);
-        cellSelect.filter(d => d.type === 'Use').text(d=>d.value);
-        cellSelect.filter(d => d.type ==='Orbit').text(d=>d.value);
+        cellSelect.filter(d => d.type === 'Use').text(d=>d.value === '' ? 'N/A' : d.value);
+        cellSelect.filter(d => d.type ==='Orbit').text(d=>d.value === '' ? 'N/A' : d.value);
+        
+
+        let weightSelect = cellSelect.filter(d => d.type === 'LWeight');
+
+        let svgSelect = weightSelect.selectAll('svg').data(d => [d])
+        .join('svg')
+        .attr('width',this.rowSvgWidht)
+        .attr('height',this.rowSvgHeight);
+        // Adding in rectangle elements
+        this.addRectangles(svgSelect);
     }
 
+    /**
+     * Adds rectangles to the chart
+     * @param {*} selection 
+     */
+    addRectangles(selection) {
+        let xScale = d3.scaleLinear().domain([0, d3.max(this.data.map(d => d['Launch Mass (kg.)']))])
+        .range([0, this.rowSvgWidth]);
+
+        selection.selectAll('rect').data(d => d)
+        .join('rect')
+        .attr('x', xScale(0))
+        .attr('y', 0)
+        .attr('width', d => {console.log(d.value); return xScale(d.value)})
+        .attr('height', 40)
+        .attr('fill', 'black');
+    }
 
     /**
      * Takes in a JSON object and translates it into separate containers 
@@ -68,7 +87,12 @@ class SatelliteTable{
             value: d['Type of Orbit']
         };
 
-        let dataList = [name, origin, use, orbit];
+        let LWeight = {
+            type: 'LWeight',
+            value: d['Launch Mass (kg.)']
+        };
+
+        let dataList = [name, origin, use, orbit, LWeight];
         return dataList;
     }
     
