20dc5064916d556670df7056c737767ddf304933
Aaron Schindler <legolover1000@gmail.com>
2022-12-03 18:02:49 -0700

    Sorting and button now (mostly) working

--- js/satelliteTable.js
+++ js/satelliteTable.js
@@ -6,21 +6,12 @@ class SatelliteTable{
      * @param {*} data JSON object of data
      */
     constructor(global_state) {
-        /**
-         * *************************************
-         * Make it so you can do two selections
-         * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-         * *************************************
-         */
-        //console.log(globalState);
         let data = global_state.satelliteData;
         this.global_state = global_state;
         this.selectedRows = [];
         this.originalData = globalState.satelliteData;
         this.data = data;
-        this.rowSvgWidth = 300;
-        this.rowSvgHeight = 50;
-        this.rowBarHeight = 40;
+        // Information for the dropdown menus
         this.dropdownData = [
             {
                 name: 'Country of Operator/Owner',
@@ -54,108 +45,120 @@ class SatelliteTable{
             }
         ];
 
+        // Information for the column headers on the table
         this.headerData = [
             {
                 sorted: false,
                 ascending: false,
                 key: 'Name of Satellite, Alternate Names',
                 id: 'SatName',
-                drop: 'DropS'
+                drop: 'DropS',
+                alterFunc: null
             },
             {
                 sorted: false,
                 ascending: false,
                 key: 'Country of Operator/Owner',
                 id: 'Country',
-                drop: 'DropCountry'
+                drop: 'DropCountry',
+                alterFunc: null
             },
             {
                 sorted: false,
                 ascending: false,
                 key: 'Contractor',
                 id: 'Contractor',
-                drop: 'DropContractor'
+                drop: 'DropContractor',
+                alterFunc: null
             },
             {
                 sorted: false,
                 ascending: false,
                 key: 'Purpose',
                 id: 'Use',
-                drop: 'DropUse'
+                drop: 'DropUse',
+                alterFunc: null
             },
             {
                 sorted: false,
                 ascending: false,
                 key: 'Type of Orbit',
                 id: 'Orbit',
-                drop: 'DropOrbit'
+                drop: 'DropOrbit',
+                alterFunc: null
             },
             {
                 sorted: false,
                 ascending: false,
                 key: 'Launch Mass (kg.)',
                 id: 'LaunchMass',
-                drop: 'DropLMass'
+                drop: 'DropLMass',
+                alterFunc: null
             },
             {
                 sorted: false,
                 ascending: false,
                 key: 'Expected Lifetime (yrs.)',
                 id: 'Lifetime',
-                drop: 'DropLifetime'
+                drop: 'DropLifetime',
+                alterFunc: null
             },
             {
                 sorted: false,
                 ascending: false,
                 key: 'Launch Site',
                 id: 'LaunchSite',
-                drop: 'DropSite'
+                drop: 'DropSite',
+                alterFunc: null
             },
             {
                 sorted: false,
                 ascending: false,
                 key: 'Launch Vehicle',
                 id: 'LaunchVehicle',
-                drop: 'DropVehicle'
+                drop: 'DropVehicle',
+                alterFunc: null
             },
             {
                 sorted: false,
                 ascending: false,
                 key: 'Dry Mass (kg.)',
                 id: 'DryMass',
-                drop: 'DropMass'
+                drop: 'DropMass',
+                alterFunc: null
             },
             {
                 sorted: false,
                 ascending: false,
-                key: 'Period (degrees)',
+                key: 'Period (minutes)',
                 id: 'Period',
-                drop: 'DropPeriod'
+                drop: 'DropPeriod',
+                alterFunc: null
             },
             {
                 sorted: false,
                 ascending: false,
                 key: 'Inclination (degrees)',
                 id: 'Inclination',
-                drop: 'DropInclination'
+                drop: 'DropInclination',
+                alterFunc: null
             },
             {
                 sorted: false,
                 ascending: false,
-                key: 'Launch Date',
+                key: 'Date of Launch',
                 id: 'LaunchDate',
-                drop: 'DropDate'
+                drop: 'DropDate',
+                alterFunc: d => new Date(d)
             }
         ]
         
         // Adding in rectangle elements
         this.buildTable();
         this.attachSortHandlers();
-        /**
-         * **************************
-         * FIX THIS
-         * **************************
-         */
+
+        // Attaching update handlers to the dropdown menus
+        // Must be done after the table is already built
         d3.select('#Country').on('change', event => this.update(event));
         d3.select('#Use').on('change', event => this.update(event));
         d3.select('#Orbit').on('change', event => this.update(event));
@@ -167,19 +170,12 @@ class SatelliteTable{
      * Fills in all cells in the table
      */
     buildTable() {
-        // Sets data based on global state grouping
-        // if(globalState.group.length > 0) {
-        //     this.data = globalState.group;
-        // }
-        // else {
-        //     this.data = globalState.satelliteData;
-        // }
+        // Retrieves the current filtered grouping from the global state
         this.data = applyGrouping();
         if(this.data.length === 0) {
             this.data = this.originalData;
         }
-        // console.log(applyGrouping());
-        // //console.log(this.data);
+
         // Stores table body selection and appends table rows
         let rowSelection = d3.select('#tableBody')
         .selectAll('tr')
@@ -220,13 +216,11 @@ class SatelliteTable{
         cellSelect.filter(d => d.type ==='Lifetime').text(d=>d.value === '' ? 'N/A' : d.value);
         cellSelect.filter(d => d.type === 'LaunchSite').text(d => d.value === '' ? 'Unknown' : d.value);
         cellSelect.filter(d => d.type === 'LaunchVehicle').text(d => d.value === '' ? 'Unknown' : d.value);
-        
         cellSelect.filter(d => d.type === 'DryMass').text(d=>d.value === '' ? 'N/A' : d.value);
         cellSelect.filter(d => d.type === 'Period').text(d=>d.value === '' ? 'N/A' : d.value);
         cellSelect.filter(d => d.type === 'Inclination').text(d=>d.value === '' ? 'N/A' : d.value);
-        cellSelect.filter(d => d.type === 'LaunchDate').text(d=>d.value ? 'N/A' : d.value);
-
-        //this.attachSortHandlers();
+        // This is a date object instead of a text element
+        cellSelect.filter(d => d.type === 'LaunchDate').text(d=>d.value === '' ? 'N/A' : new Date(d.value).toLocaleString().split(',')[0]);
     }
 
     /**
@@ -235,19 +229,20 @@ class SatelliteTable{
      */
     attachSortHandlers() 
     {
-
-        /**
-         * ***************************
-         * FIX THIS
-         * ***************************
-         */
+        // Attaching on click handler for the reset button
         d3.select('#FilterReset').on('click', event => {
-            for (let [key, value] of globalState.group) {
-                globalState.group[key] = null;
-            }; 
-            updateAllGroup()});
+            for (let el of globalState.group) {
+                el[1] = null;
+            };
+            for(let el of this.dropdownData) {
+                el.filtered = false;
+                el.filterElement = null;
+            }
+            updateAllGroup();
+            this.addFilters();
+        });
         
-
+        // Attaches a dropdown menu to the appropriate columns of the table
         const dropData = this.dropdownData.map(d => d.name);
         for(let index in this.headerData) {
             let item = this.headerData[index];
@@ -255,21 +250,20 @@ class SatelliteTable{
                 d3.select(`#${item.drop}`)
                 .attr('id', item.key)
                 .append('select')
-                .attr('id', item.id);
-
-                
+                .attr('id', item.id); 
             }
         }
         this.addFilters();
 
 
+        // Attaches a a sort handler for each column in the table
         d3.select('#columnHeaders')
             .selectAll('th')
             .data(this.headerData)
             .on('click', (event, d) => 
             {
-
-                const sortAscending = d.sorted ? !d.ascending : true; // sort ascending by default, otherwise flip it.
+                // Sort ascending by default, otherwise flip it
+                const sortAscending = d.sorted ? !d.ascending : true;
                 this.sortData(d.key, sortAscending, d.alterFunc);
                 // reset state
                 for (let header of this.headerData)
@@ -292,18 +286,7 @@ class SatelliteTable{
      * @returns 
      */
     rowToCellDataTransform(d) {
-
-        /**
-         * ********************************************
-         * DATA TO ADD
-         * ********************************************
-         * 
-         * Dry Mass
-         * Period
-         * Inclination
-         * Date of Launch
-         * 
-         */
+        // Creating data groupings
         let name = {
             type: 'Name',
             value: d['Name of Satellite, Alternate Names']
@@ -378,12 +361,13 @@ class SatelliteTable{
      * Updates the row selection
      */
     updateRows() {
+        // Builds rows in the table, appending the selected class by default
         let rowSelection = d3.select('#tableBody')
         .selectAll('tr')
         .data(this.data)
         .join('tr')
         .classed('selectedTable', true);
-
+        // If a row is selected highlight it, otherwise do not highlight it
         rowSelection.classed('selectedTable', (d) => globalState.selection.includes(d['Name of Satellite, Alternate Names']));
         rowSelection.classed('unselectedTable', (d) => !globalState.selection.includes(d['Name of Satellite, Alternate Names']))
     }
@@ -396,17 +380,19 @@ class SatelliteTable{
      */
     sortData(key, ascend, alterFunc)
     {
+        // Sorting data by key
         this.data.sort((a, b) =>
             {
                 let sortKey = key;
                 let x = a[sortKey];
                 let y = b[sortKey];
                 
-
+                // Reverse the sort
                 if (!ascend)
                 {
-                    [x, y] = [y, x] // swap variables
+                    [x, y] = [y, x]
                 }
+                // If this is a datetime object, use the alternative sorting
                 if (alterFunc)
                 {
                     x = alterFunc(x);
@@ -423,77 +409,76 @@ class SatelliteTable{
                 return 0;
             }
         );
-
+        // Send this data to the global state
         globalState.satelliteData = this.data;
         updateSort();
     }
 
+    /**
+     * Updates the table to show the data with a filter applied
+     */
     updateGroup() {
-        
         this.buildTable();
         this.updateRows();
         this.attachSortHandlers();
     }
 
+    /**
+     * Updates the list of selected elements based on user input
+     */
     updateSelection() {
         this.updateRows();
     }
 
     /**
-     * Adding in the dropdown menu filters
+     * Adds in the dropdown menu items based on current filtering
      */
     addFilters() {
-
-        /**
-         * **********************************
-         * Add on click that checks if col is already filtered
-         * and reset if it has, otherwise allow the dropdown
-         * **********************************
-         */
-        let newData = [... new Set(this.data.map(d => d['Country of Operator/Owner']))];
-        let nn = [...new Set(this.data.map(d=>d['Purpose']))];
-        //console.log(nn)
-
+        // Selecting dropdown elements
         let CountrySelect = d3.select('#Country');
         let PurposeSelect = d3.select('#Use');
         let OrbitSelect = d3.select('#Orbit');
         let LaunchSselect = d3.select('#LaunchSite');
         let LaunchVSelect = d3.select('#LaunchVehicle');
 
-        let CountryData = [...new Set(this.data.map(d => d['Country of Operator/Owner'] === '' ? 'Unknown' : d['Country of Operator/Owner'])), ' All'].sort();
-        let PurposeData = [...new Set(this.data.map(d => d['Purpose'] === '' ? 'Unknown' : d['Purpose'])), ' All'].sort();
-        let OrbitData = [...new Set(this.data.map(d => d['Type of Orbit'] === '' ? 'Unknown' : d['Type of Orbit'])), ' All'].sort();
-        let LaunchSData = [...new Set(this.data.map(d => d['Launch Site'] === '' ? 'Unknown' : d['Launch Site'])), ' All'].sort();
-        let LaunchVData = [...new Set(this.data.map(d => d['Launch Vehicle'] === '' ? 'Unknown' : d['Launch Vehicle'])), ' All'].sort();
+        // Create a set of values to be used in the dropdown menu
+        let CountryData = [...new Set(this.data.map(d => d['Country of Operator/Owner'] === '' ? 'Unknown' : d['Country of Operator/Owner'])), '', ' All'].sort();
+        let PurposeData = [...new Set(this.data.map(d => d['Purpose'] === '' ? 'Unknown' : d['Purpose'])), '', ' All'].sort();
+        let OrbitData = [...new Set(this.data.map(d => d['Type of Orbit'] === '' ? 'Unknown' : d['Type of Orbit'])), '', ' All'].sort();
+        let LaunchSData = [...new Set(this.data.map(d => d['Launch Site'] === '' ? 'Unknown' : d['Launch Site'])), '', ' All'].sort();
+        let LaunchVData = [...new Set(this.data.map(d => d['Launch Vehicle'] === '' ? 'Unknown' : d['Launch Vehicle'])), '', ' All'].sort();
 
         // If the data is filterd, set the dropdown menu to only display the selected filter and an 'all' element
         if(this.dropdownData.find(el => el.name === 'Country of Operator/Owner').filtered){
             let value = this.dropdownData.find(el => el.name === 'Country of Operator/Owner').filterElement;
-            CountryData = [value, 'All'];
+            CountryData = [value, ' All'];
+            console.log(CountryData);
         }
         if(this.dropdownData.find(el => el.name === 'Purpose').filtered){
             let value = this.dropdownData.find(el => el.name === 'Purpose').filterElement;
             console.log(value);
-            PurposeData = [value, 'All'];
+            PurposeData = [value, ' All'];
         }
         if(this.dropdownData.find(el => el.name === 'Type of Orbit').filtered){
             let value = this.dropdownData.find(el => el.name === 'Type of Orbit').filterElement;
-            OrbitData = [value, 'All'];
+            OrbitData = [value, ' All'];
         }
         if(this.dropdownData.find(el => el.name === 'Launch Site').filtered){
             let value = this.dropdownData.find(el => el.name === 'Launch Site').filterElement;
-            LaunchSData = [value, 'All'];
+            LaunchSData = [value, ' All'];
         }
         if(this.dropdownData.find(el => el.name === 'Launch Vehicle').filtered){
             let value = this.dropdownData.find(el => el.name === 'Launch Vehicle').filterElement;
-            LaunchVData = [value, 'All'];
+            LaunchVData = [value, ' All'];
         }
+        
 
+        // Updating the options in the dropdown menu
         CountrySelect
         .selectAll('option')
         .data(CountryData)
         .join('option')
-        .text(d=> d);
+        .text(d=>d);
 
         PurposeSelect
         .selectAll('option')
@@ -521,38 +506,37 @@ class SatelliteTable{
 
     }
 
+    /**
+     * Updates the global and local filtering meta data
+     * @param {*} event 
+     */
     update(event) {
-        /*
-            Work this out so it can filter multiple selections together
-            Need to have filters for Orbit and Launch Site?
-        */
-        //console.log(event);
+        // pulling the element we want to filter by and which column it is in
         let filter = d3.select(`#${event.path[0].id}`).property('value');
-        //console.log(filter);
         let tempKey = event.path[1].id;
-        //console.log(tempKey);
-        //console.log(filter);
         
-
+        // Setting the filter in the global state so each view knows what filter to use
         for(let g of globalState.group) {
             if(g[0] === tempKey) {
                 if(filter === 'All' || filter === ' All') {
                     g[1] = null;
                 }
                 else {
-                    g[1] = filter;
-                    
+                    g[1] = filter;                   
                 }
             }
         }
 
+        // Setting meta data for filtering in the local scope
         for(let el of this.dropdownData) {
             if(el.name === tempKey) {
                 if(filter === 'All') {
                     el.filtered = false;
-                    el.filtereElement = null;
+                    el.filterElement = null;
+                    console.log(el);
                 }
                 else {
+                    console.log(filter);
                     el.filtered = true;
                     el.filterElement = filter;
                 }
